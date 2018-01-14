bef381b534793f144ff37f2a69b28992d777fead
Jeff Tan <xmutww@aliyun.com>
2018-01-14 16:29:50 +0800

    init nester

---
+++ nester/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name                = 'nester',
+    version             = '1.0.0',
+    py_modules          = ['nester'],
+    author              = 'Jeff Tan',
+    author_email        = 'xmutww@aliyun.com',
+    url                 = 'http://www.jeff.tan.com',
+    description         = 'A simple printer of nested lists'
+)
